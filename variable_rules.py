2e7355be34f8fb0a957743fc6b4d55eef48d9334
pranu1111 <107885176+pranu1111@users.noreply.github.com>
2022-06-23 01:42:12 +0530

    Add files via upload

---
+++ variable_rules.py
@@ -0,0 +1,25 @@
+#1a      #SyntaxError: invalid decimal literal
+AA=5     # variables can end with numbers
+print(AA)
+bb=10
+print(bb)
+Ab=20
+print(Ab)
+bA=40
+print(bA)
+#11=80
+#print(11)..........variable cant be a only number
+A1=2.50
+print(A1)   #Number can be at end of var
+a1b=1
+print(a1b)  # Number can be at middle in var
+_a=8
+print(_a)   #we can use underscore to declare variable
+A_b=1008
+print(A_b)
+A_=8998
+print(A_)
+# 1a=14
+# print(1a)...........variable name can not start with number
+
+
